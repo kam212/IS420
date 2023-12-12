7f7f92909f63d177fb4819af00dd479f4d4db693
kam212 <144737587+kam212@users.noreply.github.com>
2023-12-11 21:54:53 -0500

    Update Feature6.sql

--- Feature6.sql
+++ Feature6.sql
@@ -1,1 +1,16 @@
 //Group Feature 6
+
+CREATE OR REPLACE PACKAGE c_package AS
+  PROCEDURE startSession(p_customer_id,
+  p_zone_id,
+  p_vehicle_id,
+  start_time,
+  hours)
+IS 
+  p_count INT;
+BEGIN
+  select(*) into p_count from customer where customer_id = p_customer_id;
+  if p_count = 0 then 
+  dbms.output.put_line(
+  
+  
