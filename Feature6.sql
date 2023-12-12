1329f5de5151c998286c6445dc7aec916bbfa2bf
kam212 <144737587+kam212@users.noreply.github.com>
2023-12-12 13:31:06 -0500

    Update Feature6.sql

--- Feature6.sql
+++ Feature6.sql
@@ -5,12 +5,19 @@ CREATE OR REPLACE PACKAGE c_package AS
   p_zone_id,
   p_vehicle_id,
   start_time,
-  hours)
+  hours
+  v_num_session out INT)
 IS 
   p_count INT;
 BEGIN
   select(*) into p_count from customer where customer_id = p_customer_id;
   if p_count = 0 then 
-  dbms.output.put_line(
+  dbms.output.put_line('Invalid customer ID');
+  else
+  select count(*) into v_num_session 
+  from parking_session
+  where customer_id = p_customer_id;
+end if;
+end;
   
   
