74cc47029493d704ffb4134197005347e1c515e6
kam212 <144737587+kam212@users.noreply.github.com>
2023-12-15 17:50:29 -0500

    Update Feature6.sql

--- Feature6.sql
+++ Feature6.sql
@@ -1,23 +1,28 @@
 //Group Feature 6
+SET SERVEROUTPUT ON;
+//*two parameters, join, check*//
+-- Create the procedure
+create or replace procedure start_session(v_customer_id varchar)
+as
+v_count int; 
 
-CREATE OR REPLACE PACKAGE c_package AS
-  PROCEDURE startSession(p_customer_id,
-  p_zone_id,
-  p_vehicle_id,
-  start_time,
-  hours
-  v_num_session out INT)
-IS 
-  p_count INT;
-BEGIN
-  select(*) into p_count from customer where customer_id = p_customer_id;
-  if p_count = 0 then 
-  dbms.output.put_line('Invalid customer ID');
-  else
-  select count(*) into v_num_session 
-  from parking_session
-  where customer_id = p_customer_id;
+cursor c1 is 
+select address,hours,start_time, end_time 
+from customer c, parking_session ps
+where c.customer_id = v_customer_id
+and c.customer_id = ps.customer_id and c.customer_id = 1
+group by ps.zone_id;
+
+begin 
+select count(*) into v_count from customer where customer_id =  v_customer_id;
+if v_count=0 then 
+    dbms_output.put_line('Invalid');
+    else
+    for r in c1 loop dbms_output.put_line('Information: ' || r.address || 'at' || r.start_time || 'to' || r.end_time ||
+    'for a total of' || r.hours);
+        end loop;
 end if;
 end;
+/
   
   
